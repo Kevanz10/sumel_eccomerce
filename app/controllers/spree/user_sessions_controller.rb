a550f9d9db4a13daec4e0f304aed78de2df81835
kevanz10 <krojas1171@gmail.com>
2017-08-23 15:39:25 -0500

    finished form

--- app/controllers/spree/user_sessions_controller.rb
+++ app/controllers/spree/user_sessions_controller.rb
@@ -12,7 +12,7 @@ class Spree::UserSessionsController < Devise::SessionsController
     if spree_user_signed_in?
       respond_to do |format|
         format.html {
-          flash[:success] = Spree.t(:logged_in_succesfully)
+          flash[:success] = Spree.t('Bienvenido')
           redirect_back_or_default(after_sign_in_path_for(spree_current_user))
         }
         format.js {
