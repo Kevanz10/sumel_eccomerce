bc61854e15728dc5f24b98a79ab6a3474f1ec5c1
kevanz10 <krojas1171@gmail.com>
2017-09-27 19:39:33 -0500

    ad

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.serve_static_files = true
+  config.public_file_server.enabled = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
